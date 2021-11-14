9880e202b9d548db48745f0c20b22ffd608abf9c
Lily Wilkinson <lily@Lilys-MacBook-Pro.local>
2021-11-14 17:03:44 +0000

    Implement vaccine uptake (one vaccination per infection)

--- network_basics.py
+++ network_basics.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 # define number of nodes and make a list of nodes
 N=100
-nodes=list(range(N))
+nodes=np.arange(0,N,1)
 
 # make a list of edges (nodes are connected to those that are close to them)
 edges=[]
@@ -44,10 +44,16 @@ beta=0.5   # the probability that an infected node infectes a susceptible neighb
 seed=0   # start with a seed node (patient zero)
 
 # generation times are drawn from the log normal distribution defined below...
-mode=5 
-dispersion=1.3
-sigma=np.log(dispersion)
-mu=(sigma**2)+np.log(mode)
+g_mode=5 
+g_dispersion=1.3
+g_sigma=np.log(g_dispersion)
+g_mu=(g_sigma**2)+np.log(g_mode)
+
+# vaccination effectiveness times are drawn from the log normal distribution defined below...
+v_mode=5 
+v_dispersion=1.3
+v_sigma=np.log(v_dispersion)
+v_mu=(v_sigma**2)+np.log(v_mode)
 
 # create a list to store the sizes of X simulated outbreaks
 X = 100000
@@ -55,12 +61,9 @@ outbreak_sizes=[]
 
 # run x simulations to collect outbreak sizes
 for i in range(1,X+1):
-    # we will keep a dictionary telling us the immunity status of each node
-    immune={}
-
-    # for initial conditions we start with all nodes susceptible
-    for node in nodes:
-        immune[node]=False
+    # we will keep an array telling us the immunity status of each node
+    # for initial conditions we start with all nodes susceptible (all values false)
+    immune=np.zeros(N, dtype=bool)
 
     # create a list of events. this list will grow and shrink over time
     events=[]
@@ -73,9 +76,9 @@ for i in range(1,X+1):
 
     # define the first event (the seeding event)
     first_infection={'type':'trans',
-                'time':0,
-                'primary':None,
-                'secondary':seed}
+                    'time':0,
+                    'primary':None,
+                    'secondary':seed}
 
     # add this to the list
     events.append(first_infection)
@@ -88,35 +91,58 @@ for i in range(1,X+1):
         event=min(events,key=lambda x: x['time'])   # fetch earliest infection event on the list
         events.remove(event)   # remove the chosen infection from the list
         
-        # ignoring cases in which the secondary is already immune (so no infection occurs)...
-        if not immune[event['secondary']]:
-            #print(str(event['primary'])+' infected '+str(event['secondary'])+' at t = '+str(event['time']))   # print the event
-            tree.append((event['primary'],event['secondary']))   # add event to the tree
+        # if the selected event is a transmission...
+        if event['type']=='trans':
+            # ignoring cases in which the secondary is already immune (so no infection occurs)...
+            if not immune[event['secondary']]:
+                #print(str(event['primary'])+' infected '+str(event['secondary'])+' at t = '+str(event['time']))   # print the event
+                tree.append((event['primary'],event['secondary']))   # add event to the tree
 
-            # now we need to add more infections to the list...
-            primary=event['secondary']   # "move on" so that the secondary becomes the new primary
-            immune[primary]=True   # make the primary immune so that no future events can affect that node
-            
-            # create new infection events to add to the list
-            for secondary in neighbours[primary]:   # for all neighbours of the primary...
-                if random.random()<beta and not immune[secondary]:   # determines if primary infects secondary
-                    generation_time=int((24*60*60)*np.random.lognormal(mu,sigma))   # how long will it be (in seconds) until primary infects secondary?
-                    transmission_time=event['time']+generation_time   # adds generation period to previous event time to give current event time
+                # now we need to add more infections to the list...
+                primary=event['secondary']   # "move on" so that the secondary becomes the new primary
+                immune[primary]=True   # make the primary immune so that no future events can affect that node
+                
+                # create new infection events to add to the list
+                for secondary in neighbours[primary]:   # for all neighbours of the primary...
+                    if random.random()<beta and not immune[secondary]:   # determines if primary infects secondary
+                        generation_time=int((24*60*60)*np.random.lognormal(g_mu,g_sigma))   # how long will it be (in seconds) until primary infects secondary?
+                        transmission_time=event['time']+generation_time   # adds generation period to previous event time to give current event time
 
-                    # create the event
-                    new_event={'type':'trans',
-                            'time':transmission_time,
-                            'primary':primary,
-                            'secondary':secondary}
-                    events.append(new_event) # add event to the list
+                        # create the event
+                        new_event={'type':'trans',
+                                    'time':transmission_time,
+                                    'primary':primary,
+                                    'secondary':secondary}
+                        events.append(new_event) # add event to the list
+
+                if len(nodes[immune==False])>0:
+                    pick_index = np.random.randint(0, len(nodes[immune==False]))
+                    pick = nodes[immune==False][pick_index]
+                    immune[pick]=True
+                    vax_event={'type':'vax',
+                                'time':transmission_time,
+                                'node':pick}
+                    events.append(vax_event)
+                    #print(pick,"got vaccinated!")
+
+        elif event['type']=='vax':
+            effective_time=int((24*60*60)*np.random.lognormal(v_mu,v_sigma))
+            end_time=event['time']+effective_time
+            end_event={'type':'unvax',
+                        'time':end_time,
+                        'node':event['node']}
+            immune[event['node']]=False
+            #print(event['node'],"become susceptible again!")
+            events.append(end_event)
             
     print("Outbreak "+str(i)+" size = "+str(len(tree)))
     outbreak_sizes.append(len(tree))   # append outbreak size to list for plotting later
 
 # plot histogram of outbreak sizes
 bin_edges = np.arange(0.5, max(outbreak_sizes)+0.5, 1)   # creates array of histogram bin edges ±0.5 of each integer outbreak size
-plt.hist(outbreak_sizes, bins=bin_edges)   # draws histogram of outbreak sizes
+plt.hist(outbreak_sizes, bins=bin_edges, label='N=100 nodes')   # draws histogram of outbreak sizes
 plt.xlabel("Outbreak size")
 plt.ylabel("Frequency")
-plt.title("Distribution of outbreak sizes for "+str(X)+" simulated outbreaks")
+plt.title("Distribution of outbreak sizes for "+str(X)+" simulated \n outbreaks, with one vaccination for each infection")
+plt.legend()
 plt.show()
