15ae50096c38b708362f90b1aa4ed279db5606a5
Lily Wilkinson <lily.wilkinson@live.co.uk>
2022-01-04 16:55:39 +0000

    Fixed random vaccination method

--- outbreak_sim.py
+++ outbreak_sim.py
@@ -8,6 +8,7 @@ import csv
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 import network as nw
 import vaccination as vx
@@ -16,6 +17,12 @@ import vaccination as vx
 
 # function to make a dictionary for any event type
 def Event(type, time, primary, secondary):
+    # each event is a small dictionary with keys...
+            # type: the type of event which occurs
+            # time: the time that the event occurs 
+            # primary: the node that is doing the infecting
+            # secondary: the node that is being infected
+
     # if the event is a transmission...
     if type=='trans':
         event={'type':type,
@@ -97,97 +104,84 @@ def main():
 
     ################################## SIMULATE OUTBREAK ##################################
     # create a list to store the sizes of X simulated outbreaks
-    X = 5000
-    outbreak_sizes=np.zeros((11,X))
+    X = 20
 
     file = open('test.csv','a')
-    file.write('Network size, N1, N2, N3, Ring 1 SMLs, Ring 2 SMLs, Ring 3 SMLs, Rings 1-2 links, Rings 1-3 links, Rings 2-3 links, Vaccination %, Outbreak size\n')
+    
+    # hack to only write header if the file is new (otherwise, just append new data)
+    if os.stat('test.csv').st_size == 0:
+        file.write('Network size, N1, N2, N3, Ring 1 SMLs, Ring 2 SMLs, Ring 3 SMLs, Rings 1-2 links, Rings 1-3 links, Rings 2-3 links, Vaccination %, Outbreak size\n')
 
-    # run simulation with 11 different vaccination amounts (0%, 10%, 20% etc to 100%)
-    for i in range(11):
-        vax_frac = int(i/10)   # (i*10)% of the total nodes will be vaccinated at random
-        print("Running for "+str(i*10)+"%...")
+    vax_frac = 0.5   # (i*10)% of the total nodes will be vaccinated at random
 
-        # run X simulations to collect outbreak sizes 
-        for j in range(X):
-            print(str(round((j*100)/X,1))+"% complete",end = "\r")
-            # we will keep an array telling us the immunity status of each node
-            # for initial conditions we start with all nodes susceptible (all values false)
-            immune=np.zeros(totalN, dtype=bool)
+    # run X simulations to collect outbreak sizes 
+    for j in range(X):
 
-            events=[]   # create a list of events (this list will grow and shrink over time)
+        immune=np.zeros(totalN, dtype=bool)   # an array telling us the immunity of each node (for initial conditions we start with all nodes susceptible)
+        tree=[]   # output is a tree-like network
 
-            # each event is a small dictionary with keys...
-            # type: the type of event which occurs
-            # time: the time that the event occurs 
-            # primary: the node that is doing the infecting
-            # secondary: the node that is being infected
+        events=[]   # create a list of events (this list will grow and shrink over time)
+        events.append(Event('trans', 0, None, seed))   # create the first transmission event (the seeding event) and add to events list
+        
+        events = vx.RandomVax(vax_frac, totalN, events)   # randomly chooses a given % of nodes to be vaccinated at a random time in the first year
+        print(len(events))
 
-            # create the first transmission event (the seeding event) and add to events list
-            events.append(Event('trans', 0, None, seed))
+        # start a loop in which we resolve the events in time order until no events remain
+        while events:
+            event=min(events,key=lambda x: x['time'])   # fetch earliest infection event on the list
+            events.remove(event)   # remove the chosen infection from the list
             
-            vx.RandomVax(vax_frac, totalN)
-            
-            # output is a tree-like network
-            tree=[]
+            # if the selected event is a transmission...
+            if event['type']=='trans':
+                # ignoring cases in which the secondary is already immune (so no infection occurs)...
+                if not immune[event['secondary']]:
+                    #print(str(event['primary'])+' infected '+str(event['secondary'])+' at '+ConvertTime(event['time']))   # print the event
+                    tree.append((event['primary'],event['secondary']))   # add event to the tree
 
-            # start a loop in which we resolve the events in time order until no events remain
-            while events:
-                event=min(events,key=lambda x: x['time'])   # fetch earliest infection event on the list
-                events.remove(event)   # remove the chosen infection from the list
-                
-                # if the selected event is a transmission...
-                if event['type']=='trans':
-                    # ignoring cases in which the secondary is already immune (so no infection occurs)...
-                    if not immune[event['secondary']]:
-                        #print(str(event['primary'])+' infected '+str(event['secondary'])+' at '+ConvertTime(event['time']))   # print the event
-                        tree.append((event['primary'],event['secondary']))   # add event to the tree
+                    # now we need to add more infections to the list...
+                    primary=event['secondary']   # "move on" so that the secondary becomes the new primary
+                    immune[primary]=True   # make the primary immune so that no future events can affect that node
+                    
+                    # create new infection events to add to the list
+                    for secondary in neighbours[primary]:   # for all neighbours of the primary...
+                        if random.random()<beta and not immune[secondary]:   # determines if primary infects secondary
+                            transmission_time = NewEventTime(event['time'], g_mu, g_sigma)   # when will the primary infect the secondary?
+                            events.append(Event('trans', transmission_time, primary, secondary))   # creates the transmission event and adds to list
 
-                        # now we need to add more infections to the list...
-                        primary=event['secondary']   # "move on" so that the secondary becomes the new primary
-                        immune[primary]=True   # make the primary immune so that no future events can affect that node
-                        
-                        # create new infection events to add to the list
-                        for secondary in neighbours[primary]:   # for all neighbours of the primary...
-                            if random.random()<beta and not immune[secondary]:   # determines if primary infects secondary
-                                transmission_time = NewEventTime(event['time'], g_mu, g_sigma)   # when will the primary infect the secondary?
-                                events.append(Event('trans', transmission_time, primary, secondary))   # creates the transmission event and adds to list
+                            resusceptible_time = NewEventTime(transmission_time, c_mu, c_sigma)
+                            events.append(Event('resusceptible', resusceptible_time, secondary, None))
 
-                                resusceptible_time = NewEventTime(transmission_time, c_mu, c_sigma)
-                                events.append(Event('resusceptible', resusceptible_time, secondary, None))
+                # if there are no more transmission events in the events list...
+                if len(list(filter(lambda e: e['type'] == 'trans', events)))==0:
+                    lastinfection = event['time']   # store the time of the final transmission
 
-                    # if there are no more transmission events in the events list...
-                    if len(list(filter(lambda e: e['type'] == 'trans', events)))==0:
-                        lastinfection = event['time']   # store the time of the final transmission
+            # if the earliest remaining event is a vaccination...
+            elif event['type']=='vax':
+                immune[event['node']]=True   # makes the vaxxed node immune
+                #print(event['node'],"got vaccinated at",ConvertTime(event['time']))
 
-                # if the earliest remaining event is a vaccination...
-                elif event['type']=='vax':
-                    immune[event['node']]=True   # makes the vaxxed node immune
-                    #print(event['node'],"got vaccinated at",ConvertTime(event['time']))
+                end_time = NewEventTime(event['time'], v_mu, v_sigma)
+                events.append(Event('unvax', end_time, event['node'], None))   # creates 'unvax' event and adds to list
 
-                    end_time = NewEventTime(event['time'], v_mu, v_sigma)
-                    events.append(Event('unvax', end_time, event['node'], None))   # creates 'unvax' event and adds to list
+            elif event['type']=='unvax':
+                immune[event['node']]=False   # node is no longer immune
+                #print(event['node'], "became re-susceptible after vaccination at", ConvertTime(event['time']))
 
-                elif event['type']=='unvax':
-                    immune[event['node']]=False   # node is no longer immune
-                    #print(event['node'], "became re-susceptible after vaccination at", ConvertTime(event['time']))
+            elif event['type']=='resusceptible':
+                immune[event['node']]=False
+                #print(event['node'], "became re-susceptible after infection at", ConvertTime(event['time']))
 
-                elif event['type']=='resusceptible':
-                    immune[event['node']]=False
-                    #print(event['node'], "became re-susceptible after infection at", ConvertTime(event['time']))
+        infected = []
+        for x in range(len(tree)):
+            infected.append(tree[x][1])
 
-            infected = []
-            for x in range(len(tree)):
-                infected.append(tree[x][1])
+        print("--- OUTBREAK WITH "+ str(int(vax_frac*totalN/10)) + "% VACCINATION STATS ---")
 
-            #print("--- OUTBREAK "+str(i+1)+" (" + str(i*10) + "% VACCINATION) STATS ---")
-
-            #print("Number of infections: "+str(len(tree)))
-            #print("Number of nodes infected: "+str(len(set(infected))))
-            #print("Last infection occurred at "+str(ConvertTime(lastinfection))+"\n")
-            
-            outbreak_sizes[i][j]=len(tree)   # append vaxxed outbreak size to list for plotting later
-            file.write(str(totalN)+","+str(N1)+","+str(N2)+","+str(N3)+",1,1,1,"+str(link1to2)+","+str(link1to3)+","+str(link2to3)+","+str(i*10)+","+str(len(tree))+"\n")
+        print("Number of infections: "+str(len(tree)))
+        print("Number of nodes infected: "+str(len(set(infected))))
+        print("Last infection occurred at "+str(ConvertTime(lastinfection))+"\n")
+        
+        file.write(str(totalN)+","+str(N1)+","+str(N2)+","+str(N3)+",1,1,1,"+str(link1to2)+","+str(link1to3)+","+str(link2to3)+","+str(vax_frac*100/totalN)+","+str(len(tree))+"\n")
 
     file.close()
 
--- vaccination.py
+++ vaccination.py
@@ -1,11 +1,58 @@
 import numpy as np
+import random
 
-def RandomVax(fraction, totalN):
-    vax_events=fraction*totalN
-    # picking a node to vaccinate....
+
+def Event(type, time, primary, secondary):
+    # each event is a small dictionary with keys...
+            # type: the type of event which occurs
+            # time: the time that the event occurs 
+            # primary: the node that is doing the infecting
+            # secondary: the node that is being infected
+
+    # if the event is a transmission...
+    if type=='trans':
+        event={'type':type,
+                'time':time,
+                'primary':primary,   # primary is the node that does the infecting
+                'secondary':secondary}   # secondary is the node that is infected
+
+    # if the event is a vaccination, 'unvax' (vaccination wearing off) or "resusceptible" (post-covid immunity wearing off)...
+    else:
+        event={'type':type,
+                'time':time,
+                'node':primary}   # primary is used to indicate the node (secondary will be passed as None)
+
+    return event
+
+
+# this was the default vaccination method used in outbreak_sim.py before 21/12
+def RandomVax(fraction, totalN, events):
+    vaxevents_no=int(fraction*totalN)
     picked=np.zeros(totalN, dtype=bool)   # starts with an array of all "false" (unvaccinated)
-    for x in range(vax_events):
+
+    # picking nodes to vaccinate....
+    for x in range(vaxevents_no):
         pick = random.choice(list(enumerate(picked[picked==False])))   # picks a random unvaccinated node
         picked[pick[0]] = True
         vax_time = np.random.randint(0,31536000)   # picks a random second within the first year to vaccinate
+        events.append(Event('vax', vax_time, pick[0], None))   # creates a vax event and adds to the list
+
+    return events
+
+
+def AgeWaveVax(N1frac, N2frac, N3frac, N1, N2, N3):
+    picked=np.zeros(N1+N2+N3, dtype=bool)   # starts with an array of all "false" (unvaccinated)
+
+    # real time from first UK case to first UK vaccination (for 65+) was ~11 months
+    N3_mode = 100
+    N3_dispersion = 60
+    N3_sigma, N3_mu = LogNormal(N3_mode, N3_dispersion)
+
+    # picking ring 3 nodes to vaccinate first...
+    for x in range(int(N3frac*N3)):
+        pick = 0
+        while pick < N1+N2:
+            pick = random.choice(list(enumerate(picked[picked==False])))   # keeps picking until the pick is in ring 3
+        picked[pick[0]] = True
+        vax_time = NewEventTime(0, N3_mu, N3_sigma)   # picks a random second within the first year to vaccinate
         events.append(Event('vax', vax_time, pick[0], None))   # creates a vax event and adds to the list
